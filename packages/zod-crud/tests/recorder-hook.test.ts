05019501d88258f89c6dee274b5e21555783f9ef
유용태 <teo.v@navercorp.com>
2026-05-18 23:00:12 +0900

    chore: update workspace changes
    2 files changed, 5 insertions(+)

--- packages/zod-crud/tests/recorder-hook.test.ts
+++ packages/zod-crud/tests/recorder-hook.test.ts
@@ -7,6 +7,10 @@ import { z } from "zod";
 import { useJSONDocument } from "../src/hooks/useJSONDocument.js";
 import { useRecorder } from "../src/sidecars/recorder.js";
 
+declare global {
+  var IS_REACT_ACT_ENVIRONMENT: boolean;
+}
+
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 
 const Schema = z.object({
